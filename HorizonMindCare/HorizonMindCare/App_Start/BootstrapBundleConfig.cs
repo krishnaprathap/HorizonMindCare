ab681630b662f1e6ac89b61b436589180b9992be
Krishna Prathap <krishh.prathap@gmail.com>
2017-05-23 04:26:27 -0700

    MVC Solution is created.

---
+++ HorizonMindCare/HorizonMindCare/App_Start/BootstrapBundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+namespace HorizonMindCare
+{
+    public class BootstrapBundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/library/mvchelper").Include("~/Scripts/plugins/rrdl-theme/rrdl-dls-theme-1.3.6.9.min.js"));
+            bundles.Add(new StyleBundle("~/Content/allcss").Include("~/Content/extras/*.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/alljs").Include("~/Scripts/*.js"));
+
+
+
+
+        }
+    }
+}
---
+++ HorizonMindCare/HorizonMindCare/Scripts/Extra/Personal.js
@@ -0,0 +1,153 @@
+﻿
+
+
+function numbersonly(e) {
+    var unicode = e.charCode ? e.charCode : e.keyCode
+    if (unicode != 8) { //if the key isn't the backspace key (which we should allow)
+        if (unicode < 48 || unicode > 57) //if not a number
+            return false //disable key press
+    }
+}
+
+function ValidateAlpha(evt) {
+    console.log("String validation ".evt)
+    var keyCode = (evt.which) ? evt.which : evt.keyCode
+    if (keyCode != 8) {
+        if ((keyCode < 65 || keyCode > 90) && (keyCode < 97 || keyCode > 123) && (keyCode < 8 || keyCode > 32))
+
+            return false;
+    }
+}
+
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("#contact").on('click', function (event) {
+        // alert("this ");
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                // scrollTop: $(hash).offset().top
+                scrollTop: (3200)
+            }, 900, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                // window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+    $("#route").on('click', function (event) {
+        // alert("this ");
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                // scrollTop: $(hash).offset().top
+                scrollTop: (2913)
+            }, 900, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                // window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+    $("#latest").on('click', function (event) {
+        // alert("this ");
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                // scrollTop: $(hash).offset().top
+                scrollTop: (1800)
+            }, 900, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                // window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    $("#spec").on('click', function (event) {
+        // alert("this ");
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                // scrollTop: $(hash).offset().top
+                scrollTop: (710)
+            }, 900, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                // window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+    $("#about").on('click', function (event) {
+        // alert("this ");
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                // scrollTop: $(hash).offset().top
+                scrollTop: (0)
+            }, 900, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                // window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    $('.carousel').carousel({
+        interval: 2000
+    })
+
+    var selector = '.nav li a';
+
+    $(selector).on('click', function () {
+        $(selector).removeClass('active');
+        $(this).addClass('active');
+    });
+});
