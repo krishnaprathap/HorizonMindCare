27f2690fe82469bb5163a987724c64cfed11bf26
Dhananjay Udaganve <DUdaganve@gategourmet.com>
2017-05-24 12:52:05 +0530

    changed code for w3c compatible

--- HorizonMindCare/HorizonMindCare/Scripts/Extra/Personal.js
+++ HorizonMindCare/HorizonMindCare/Scripts/Extra/Personal.js
@@ -139,6 +139,28 @@ $(document).ready(function () {
             });
         } // End if
     });
+    $("#MobileAbout").on('click', function (event) {
+        // alert("this ");
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                // scrollTop: $(hash).offset().top
+                scrollTop: (0)
+            }, 900, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                // window.location.hash = hash;
+            });
+        } // End if
+    });
 
     $('.carousel').carousel({
         interval: 2000
