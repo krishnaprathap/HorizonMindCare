7bff5161e2ffe7dc40e3f9d5c2a2541a42df997d
chandureddy9045 <chandra.reddy@saggezza.com>
2017-06-28 13:07:12 +0530

    added the facilty links and pages.

---
+++ HorizonMindCare/HorizonMindCare/Controllers/FacilitiesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HorizonMindCare.Controllers
+{
+    public class FacilitiesController : Controller
+    {
+
+        public ActionResult Academic()
+        {
+            return View("Academic");
+        }
+        public ActionResult Addictions()
+        {
+            return View("Addictions");
+        }
+        public ActionResult ChildProblem()
+        {
+            return View("ChildProblem");
+        }
+        public ActionResult MemoryProblems()
+        {
+            return View("MemoryProblems");
+        }
+        public ActionResult Psychosomaticdisorders()
+        {
+            return View("Psychosomaticdisorders");
+        }
+        public ActionResult SexualProblems()
+        {
+            return View("SexualProblems");
+        }
+        public ActionResult StressinRelationship()
+        {
+            return View("StressinRelationship");
+        }
+        public ActionResult WomenpsychologicalProblems()
+        {
+            return View("WomenpsychologicalProblems");
+        }
+        
+	}
+}
