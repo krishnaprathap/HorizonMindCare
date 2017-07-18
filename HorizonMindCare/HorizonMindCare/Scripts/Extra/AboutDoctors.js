e116d59a2c040f0e4585b6173ffb994d44a202a7
chandureddy9045 <chandra.reddy@saggezza.com>
2017-07-18 15:38:48 +0530

    images

---
+++ HorizonMindCare/HorizonMindCare/Scripts/Extra/AboutDoctors.js
@@ -0,0 +1,17 @@
+﻿var id = @ViewBag.id;
+alert("ID From Route Data: " + id);
+
+
+function edit() {
+    if (id ==1) {
+
+        var x = document.getElementById("kaveri");
+        x.style.display = 'none';
+
+        var y = document.getElementById("Nikhil");
+        y.style.display = 'none';
+    }
+}
+
+
+
