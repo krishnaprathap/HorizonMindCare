4a5e0a86aa4fadd798ed1b029d64e27bba01f640
chandureddy9045 <chandra.reddy@saggezza.com>
2017-08-01 15:45:26 +0530

    json changes for latest news

---
+++ HorizonMindCare/HorizonMindCare/Controllers/ReadDataController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using HorizonMindCare.Models;
+
+namespace HorizonMindCare.Controllers
+{
+    public class ReadDataController : Controller
+    {
+        // GET: ReadData
+        public ActionResult Index()
+        {
+
+            string file = Server.MapPath("~/Content/hi.txt");
+            string json = System.IO.File.ReadAllText(file);
+          
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            var personlist = ser.Deserialize<List<News>>(json);
+            return View(personlist);
+        }
+
+        [HttpGet]
+        public JsonResult news()
+        {
+            string file = Server.MapPath("~/Content/hi.txt");
+            string json = System.IO.File.ReadAllText(file);
+           
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            var newslist = ser.Deserialize<List<News>>(json);
+            
+            
+            return Json(newslist, JsonRequestBehavior.AllowGet);
+
+
+        }
+    }
+}
---
+++ HorizonMindCare/HorizonMindCare/Models/News.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HorizonMindCare.Models
+{
+    public class News
+    {
+        public int NewsId
+        {
+            get;
+            set;
+        }
+        public string ImpNews
+        {
+            get;
+            set;
+        }
+        
+      public DateTime date
+        {
+
+            get;
+            set;
+        }
+    }
+}
